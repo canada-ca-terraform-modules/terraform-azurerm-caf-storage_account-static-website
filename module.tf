faf5032a6a7cd802bec11545450f4ae0ab64e188
Maxime Mahdavian <maxime.mahdavian@ssc-spc.gc.ca>
2024-11-14 14:15:48 -0500

    Removed options for AAD group instead using L2 SP to connect to storage account with a pipeline

--- module.tf
+++ module.tf
@@ -25,28 +25,12 @@ module "front_door" {
     tags = var.tags
 }
 
-data "azuread_users" "sa-access-group-owners" {
-  count = try(var.static_websites.aad_group, null) != null ? 1 : 0
-  user_principal_names = var.static_websites.aad_group.owners
+data "azuread_service_principal" "devops-sp" {
+  display_name = "${var.env}_${var.group}_${var.project}_devops_sp"
 }
 
-data "azuread_users" "sa-access-group-members" {
-  count = try(var.static_websites.aad_group, null) != null ? 1 : 0
-  user_principal_names = var.static_websites.aad_group.members
-}
-
-resource "azuread_group" "sa-access-group" {
-  count = try(var.static_websites.aad_group, null) != null ? 1 : 0
-  display_name = "${var.env}-${var.group}-${var.project}-static-website-access"
-  owners = data.azuread_users.sa-access-group-owners[0].object_ids
-  members = data.azuread_users.sa-access-group-members[0].object_ids
-  security_enabled = true
-  prevent_duplicate_names = true
-}
-
-resource "azurerm_role_assignment" "sa-blob-contributor" {
-  count = try(var.static_websites.aad_group, null) != null ? 1 : 0
+resource "azurerm_role_assignment" "blob-contributor-devops-sp" {
   scope = module.storage-account.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id = azuread_group.sa-access-group[0].id
+  principal_id = data.azuread_service_principal.devops-sp.id
 }
