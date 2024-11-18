32e0397444d1191a9341b1e4713cf8c73507c428
Maxime Mahdavian <maxime.mahdavian@ssc-spc.gc.ca>
2024-11-18 08:43:04 -0500

    removing azuread in blueprint template

--- L2_portal_static_website/modules/main.tf
+++ L2_portal_static_website/modules/main.tf
@@ -5,11 +5,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0.0"
     }
-    azuread = {
-      # https://github.com/hashicorp/terraform-provider-azuread
-      source  = "hashicorp/azuread"
-      version = "~> 2.22.0"
-    }
   }
   required_version = "= 1.9.1"
 }
